842fa1c0cc239b0c0956ebf0903a2736ec7a701a
Jared S <jaredjames2020@gmail.com>
2019-10-15 16:19:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,31 @@
+
 require 'pry'
-require_relative 'concerns/findable.rb'
-require_relative 'concerns/memorable.rb'
 
 class Song
-  extend Findable, Memorable
   attr_accessor :name
-  attr_reader :artist
+  attr_reader :artist, :cheater
 
-  @@all = []
+  @@songs = []
 
   def initialize
-    @@all << self
+    @@songs << self
   end
 
   def self.find_by_name(name)
-    @@all.detect{|a| a.name == name}
+    @@songs.detect{|a| a.name == name}
   end
 
-  # def self.all
-  #   @@songs
-  # end
+  def self.all
+    @@songs
+  end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
+  def self.reset_all
+    self.all.clear
+  end
 
-  # def self.count
-  #   self.all.count
-  # end
+  def self.count
+    self.all.count
+  end
 
   def artist=(artist)
     @artist = artist
